cd00edf01214e8350847b516ce4e15fd7424b073
cyberpuffgirl <cyberpuffgirl.dev@gmail.com>
2022-07-08 15:08:44 -0600

    Most recent solution

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,19 @@
-const repeatString = function() {
+const repeatString = function(str, num){
+// will store final return //
+    let result = []; 
 
+if (num < 0) {
+    return 'ERROR'
+} else {
+    // loop will keep returning the str up until the num //
+    for (let number = 0; number < num; number++) {
+    // result will store each str iteration //
+    result.push(str);
+    }
+}
+
+
+return result.join('');
 };
 
 // Do not edit below this line
