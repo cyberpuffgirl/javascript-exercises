9de70a1e6a5c58a2df9acdb33ee6f8fa99967614
cyberpuffgirl <cyberpuffgirl.dev@gmail.com>
2022-07-11 19:26:07 -0600

    Most recent solution

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,23 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+let arr = []; 
+let sum = 0; 
+
+if (start < 0 || end < 0 || typeof start !== 'number'|| typeof end !== 'number') {
+    return 'ERROR'
+} else if (start > end) {
+    for (let i = end; i <= start; i++) {
+        arr.push(i)
+    }
+} else {
+    for (let i = start; i <= end; i++) {
+    arr.push(i)
+    }
+}
+
+for (let elem of arr) {
+    sum += elem; 
+}
+return sum;
 
 };
 
